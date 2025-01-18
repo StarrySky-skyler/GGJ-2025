09342eaa8b166f7e60d20674bab44af4946f8d19
绘星tsuki <xiaoyuesun915@gmail.com>
2025-01-18 15:18:13 +0800

    [add] 音效，bgm，UI天气预报
    Signed-off-by: 绘星tsuki <xiaoyuesun915@gmail.com>

---
+++ Assets/Scripts/Tsuki/Managers/AudioManager.cs
@@ -0,0 +1,92 @@
+﻿// ********************************************************************************
+// @author: Starry Sky
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/01/18 14:01
+// @version: 1.0
+// @description:
+// ********************************************************************************
+
+using System;
+using Tsuki.Weather;
+using UnityEngine;
+
+namespace Tsuki.Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+        
+        [Header("音频配置")]
+        public AudioSource bgmAudioSource;
+        public AudioSource soundEffectAudioSource;
+        public AudioSource weatherAudioSource;
+
+        private void Awake()
+        {
+            Instance = this;
+            soundEffectAudioSource.loop = false;
+            weatherAudioSource.loop = false;
+        }
+
+        private void Start()
+        {
+            // 注册事件
+            WeatherManager.Instance.OnSeasonChanged += PlayBgm;
+        }
+
+        /// <summary>
+        /// 播放季节bgm
+        /// </summary>
+        /// <param name="season"></param>
+        private void PlayBgm(SeasonTye season)
+        {
+            bgmAudioSource.Stop();
+            AudioClip clip = Resources.Load<AudioClip>("AudioClips/Bgm/春");
+            switch (season)
+            {
+                case SeasonTye.Spring:
+                    clip = Resources.Load<AudioClip>("AudioClips/Bgm/春");
+                    break;
+                case SeasonTye.Summer:
+                    clip = Resources.Load<AudioClip>("AudioClips/Bgm/夏");
+                    break;
+                case SeasonTye.Autumn:
+                    clip = Resources.Load<AudioClip>("AudioClips/Bgm/秋");
+                    break;
+                case SeasonTye.Winter:
+                    clip = Resources.Load<AudioClip>("AudioClips/Bgm/冬");
+                    break;
+            }
+            bgmAudioSource.clip = clip;
+            bgmAudioSource.Play();
+        }
+
+        /// <summary>
+        /// 播放天气音效
+        /// </summary>
+        /// <param name="weather"></param>
+        public void PlaySoundEffects(WeatherType weather)
+        {
+            AudioClip clip;
+            switch (weather)
+            {
+                case WeatherType.Hurricane:
+                    clip = Resources.Load<AudioClip>("AudioClips/SoundEffects/风");
+                    soundEffectAudioSource.PlayOneShot(clip);
+                    break;
+                case WeatherType.Rainy:
+                    clip = Resources.Load<AudioClip>("AudioClips/SoundEffects/雨");
+                    soundEffectAudioSource.PlayOneShot(clip);
+                    break;
+                case WeatherType.Lightning:
+                    clip = Resources.Load<AudioClip>("AudioClips/SoundEffects/闪电");
+                    soundEffectAudioSource.PlayOneShot(clip);
+                    break;
+                case WeatherType.Hail:
+                    clip = Resources.Load<AudioClip>("AudioClips/SoundEffects/冰雹");
+                    soundEffectAudioSource.PlayOneShot(clip);
+                    break;
+            }
+        }
+    }
+}
--- Assets/Scripts/Tsuki/Managers/UIManager.cs
+++ Assets/Scripts/Tsuki/Managers/UIManager.cs
@@ -9,6 +9,7 @@
 using System;
 using Tsuki.Weather;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Tsuki.Managers
 {
@@ -16,9 +17,80 @@ namespace Tsuki.Managers
     {
         public static UIManager Instance { get; private set; }
 
+        [Header("UI配置")] public Text txtWeather;
+        public Text txtSeason;
+
         private void Awake()
         {
             Instance = this;
+        }
+
+        private void Start()
+        {
+            // 注册事件
+            WeatherManager.Instance.OnSeasonChanged += UpdateSeasonUI;
+        }
+
+        private void Update()
+        {
+            UpdateWeatherUI();
+        }
+
+        /// <summary>
+        /// 更新季节UI
+        /// </summary>
+        /// <param name="season"></param>
+        private void UpdateSeasonUI(SeasonTye season)
+        {
+            txtSeason.text = "当前季节：";
+            switch (season)
+            {
+                case SeasonTye.Spring:
+                    txtSeason.text += "春";
+                    break;
+                case SeasonTye.Summer:
+                    txtSeason.text += "夏";
+                    break;
+                case SeasonTye.Autumn:
+                    txtSeason.text += "秋";
+                    break;
+                case SeasonTye.Winter:
+                    txtSeason.text += "冬";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 更新天气UI
+        /// </summary>
+        private void UpdateWeatherUI()
+        {
+            txtWeather.text = "天气预报：接下来预计是\n";
+            if (WeatherManager.Instance.CurrentWeathers.Count == 0) txtWeather.text += "晴天";
+            else
+            {
+                foreach (WeatherType weather in WeatherManager.Instance.CurrentWeathers)
+                {
+                    switch (weather)
+                    {
+                        case WeatherType.Hurricane:
+                            txtWeather.text += " 飓风";
+                            break;
+                        case WeatherType.Rainy:
+                            txtWeather.text += " 雨天";
+                            break;
+                        case WeatherType.Fog:
+                            txtWeather.text += " 大雾";
+                            break;
+                        case WeatherType.Lightning:
+                            txtWeather.text += " 雷电";
+                            break;
+                        case WeatherType.Hail:
+                            txtWeather.text += " 冰雹";
+                            break;
+                    }
+                }
+            }
         }
     }
 }
